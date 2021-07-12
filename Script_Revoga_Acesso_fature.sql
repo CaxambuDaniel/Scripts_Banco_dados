b8957bb17b3707a6bcb31949c482beb6fa1069d9
Daniel Carvalho <32563295+CaxambuDaniel@users.noreply.github.com>
2021-07-12 10:38:42 -0300

    adição de cometário

--- Script_Revoga_Acesso_fature.sql
+++ Script_Revoga_Acesso_fature.sql
@@ -1,6 +1,6 @@
 USE Dativa;
 
---CIRA«√O DE UMA TABELA QUE ARMAZENAR¡ OS LOGINS ¿ SEREM INATIVADOS E OS CAMPOS QUE SER√O ALTERADOS (a idÈia È utilizar essa tabela, tanto como parametro para alteraÁ„o quanto para backup)
+--CIRA√á√ÉO DE UMA TABELA QUE ARMAZENAR√Å OS LOGINS √Ä SEREM INATIVADOS E OS CAMPOS QUE SER√ÉO ALTERADOS (a id√©ia √© utilizar essa tabela, tanto como parametro para altera√ß√£o quanto para backup)
 CREATE TABLE inativa_logins
 			(id_login int,
 			 fl_LoginAtivo int, 
@@ -11,14 +11,14 @@ CREATE TABLE inativa_logins
 			 fl_troca_senha int
 			 );	
 
---INSER«√O DOS LOGINS ¿ SEREM INATIVADOS NA TABELA CRIADA ACIMA
+--INSER√á√ÉO DOS LOGINS √Ä SEREM INATIVADOS NA TABELA CRIADA ACIMA
 INSERT INTO inativa_logins (id_login, fl_LoginAtivo, fl_EmailValido, fl_ReCad, fl_travado, fl_status, fl_troca_senha)
 			(SELECT id_login,fl_LoginAtivo, fl_EmailValido, fl_ReCad, fl_travado, fl_status, fl_troca_senha
 			 FROM TB_FRA_LOGIN
-			 WHERE id_login in (ids_login aqui)
+			 WHERE id_login in (--ids_login aqui)
 			 );
 
---ALTERA«√O NA TABELA EM PRODU«√O (Visando melhora no desempenho, utilizo os logins inseridos na tabela 'inatica_logins' como par‚metro de busca para as inativaÁıes na tabela em produÁ„o)
+--ALTERA√á√ÉO NA TABELA EM PRODU√á√ÉO (Visando melhora no desempenho, utilizo os logins inseridos na tabela 'inatica_logins' como par√¢metro de busca para as inativa√ß√µes na tabela em produ√ß√£o)
 UPDATE TB_FRA_LOGIN 
 SET fl_LoginAtivo = 0, fl_EmailValido = 0, fl_ReCad = 0, fl_travado = 1, fl_status = 0, fl_troca_senha = 0
 --SELECT * FROM TB_FRA_LOGIN
